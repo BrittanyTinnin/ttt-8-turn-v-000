b31d97c7ab500f4b8c64ef10aa4f3ec15b631890
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-03 21:31:20 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,9 @@ def move(board, index, value="X")
 end
 
 def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0, 8) ? true : false
 end
 
 def position_taken?(board, index)
-  (board[index])
+  (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true)
+end
