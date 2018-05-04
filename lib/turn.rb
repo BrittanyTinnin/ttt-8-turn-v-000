6455945dbcbcd6ebad6e5afeab543d355c3e7768
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-04 02:27:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,7 +27,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   valid_move?(board, index)
-  index = 0
+  index = 4
   if !valid_move?(board, index)
     turn(board)
   end
